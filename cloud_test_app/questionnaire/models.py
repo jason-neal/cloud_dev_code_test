7aa9587f7aec72ae7dda0087f3886df0fc1bc55b
Jason Neal <jason.neal@astro.up.pt>
2019-03-01 23:15:56 +1300

    Rearrange Choices and add default.

--- cloud_test_app/questionnaire/models.py
+++ cloud_test_app/questionnaire/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 
+JAN, FEB, MAR, APR, MAY, JUN = 1, 2, 3, 4, 5, 6
+JUL, AUG, SEP, OCT, NOV, DEC = 7, 8, 9, 10, 11, 12
+MONTHS= ((JAN, "January"),
+         (FEB, "February"),
+         (MAR, "March"),
+         (APR, "April"),
+         (MAY, "May"),
+         (JUN, "June"),
+         (JUL, "July"),
+         (AUG, "August"),
+         (SEP, "September"),
+         (OCT, "October"),
+         (NOV, "November"),
+         (DEC, "December"),
+         )
+
+MON, TUE, WED, THU, FRI, SAT, SUN = 1, 2, 3, 4, 5, 6, 7
+DAYS = ((MON, "Monday"),
+        (TUE, "Tuesday"),
+        (WED, "Wednesday"),
+        (THU, "Thursday"),
+        (FRI, "Friday"),
+        (SAT, "Saturday"),
+        (SUN, "Sunday"), )
+
+
 class FilledQuestionnaire(models.Model):
     """FilledQuestionnaire model for cloud_test app.
 
     Stores the months and days in database by their order number.
     Uses choices to enable form selction by name.
     """
-    MONTHS= ((1, "January"),
-              (2,"February"),
-              (3,"March"),
-              (4,"April"),
-              (5,"May"),
-              (6,"June"),
-              (7,"July"),
-              (8,"August"),
-              (9,"September"),
-              (10,"October"),
-              (11,"November"),
-              (12,"December"),)
-    DAYS = ((1, "Monday"),
-            (2, "Tuesday"),
-            (3, "Wednesday"),
-            (4, "Thursday"),
-            (5, "Friday"),
-            (6, "Saturday"),
-            (7, "Sunday"), )
 
-    month = models.IntegerField(choices=MONTHS)
-    day = models.IntegerField(choices=DAYS)
+    month = models.IntegerField(choices=MONTHS, default=JAN)
+    day = models.IntegerField(choices=DAYS, default=MON)
