e67f44c70b3d50d236814237f167fbc597984078
Jason Neal <jason.neal@astro.up.pt>
2019-03-01 23:15:25 +1300

    Use forms.Model to use model Choices

--- cloud_test_app/questionnaire/forms.py
+++ cloud_test_app/questionnaire/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from questionnaire.models import FilledQuestionnaire
 
 
-class QuestionnaireForm(forms.Form):
-    month = forms.IntegerField()
-    day = forms.IntegerField()
+class QuestionnaireForm(forms.ModelForm):
+    class Meta:
+        model = FilledQuestionnaire
+        fields = '__all__'
